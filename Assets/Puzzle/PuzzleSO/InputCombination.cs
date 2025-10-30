18b9586c235900faa237802d5169b82949908996
RichardOhata <richardohata@gmail.com>
2025-10-29 17:37:56 -0700

    Implemented 1/3 input combination puzzle

---
+++ Assets/Puzzle/PuzzleSO/InputCombination.cs
@@ -0,0 +1,169 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputCombination : Puzzle
+{
+    public enum PuzzleID
+    {
+        West,
+        East,
+        North,
+        South
+    }
+
+    [SerializeField] public PuzzleID instanceID;
+
+    [SerializeField] public GameObject[] combinationComponents;
+
+    [SerializeField] public GameObject currentComponent;
+
+    public List<int> correctCombination;
+
+    [SerializeField] private GameObject rightArrow;
+    [SerializeField] private GameObject downArrow;
+
+    public event System.Action<InputCombination> OnComponentRotated;
+    public override void StartPuzzle()
+    {
+        base.StartPuzzle();
+       
+    }
+    public override void SolvePuzzle()
+    {
+        base.SolvePuzzle();
+        puzzleData.isSolved = true;
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (instanceID == PuzzleID.West)
+        {
+            SetCombination(new List<(int, int)>
+        {
+            (0, 6), 
+            (2, 7)  
+        });
+        }
+    }
+
+    /// <summary>
+    /// Set specific combination values using index-value pairs.
+    /// Example usage:
+    /// SetCombination(new List<(int, int)> { (3, 5), (1, 2) });
+    /// </summary>
+    public void SetCombination(List<(int index, int value)> indexValuePairs)
+    {
+        foreach (var (index, value) in indexValuePairs)
+        {
+            if (index < 0 || index >= combinationComponents.Length)
+            {
+                Debug.LogWarning($"Invalid index {index} in SetCombination");
+                continue;
+            }
+
+            var component = combinationComponents[index];
+            if (component == null) continue;
+
+            var cylinder = component.GetComponent<CombinationCylinder>();
+            if (cylinder != null)
+            {
+                cylinder.SetCurrentValue(value);
+            }
+            else
+            {
+                Debug.LogWarning($"{component.name} has no CombinationCylinder attached!");
+            }
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 3f)) // 3f = ray distance
+            {
+                GameObject hitObject = hit.collider.gameObject;
+
+                if (hitObject == rightArrow)
+                {
+                    Debug.Log("Looking at right arrow!");
+                    RotateCombinationComponent();
+                }
+                else if (hitObject == downArrow)
+                {
+                    Debug.Log("Looking at down arrow!");
+                    MoveNextComponent();
+                }
+            }
+        }
+    }
+
+    // Moves horizontally to the next component
+    private void RotateCombinationComponent()
+    {
+       
+        currentComponent.GetComponent<CombinationCylinder>().Rotate();
+        OnComponentRotated?.Invoke(this); // Executes different functions after it rotates a combianation component
+        CheckCombination();
+    }
+
+
+    // Moves vertically to the next component
+    private void MoveNextComponent()
+    {
+        currentComponent.GetComponent<Outline>().enabled = false;
+        int currentIndex = System.Array.IndexOf(combinationComponents, currentComponent);
+        currentIndex = (currentIndex + 1) % combinationComponents.Length;
+        currentComponent = combinationComponents[currentIndex];
+        currentComponent.GetComponent<Outline>().enabled = true;
+    }
+
+    /// <summary>
+    /// Returns a list of the current values from all combination components.
+    /// </summary>
+    public List<int> GetCurrentCombination()
+    {
+        List<int> currentValues = new List<int>();
+
+        foreach (GameObject component in combinationComponents)
+        {
+            if (component == null)
+            {
+                currentValues.Add(-1); // or skip, depending on preference
+                continue;
+            }
+
+            CombinationCylinder cylinder = component.GetComponent<CombinationCylinder>();
+            if (cylinder != null)
+            {
+                currentValues.Add(cylinder.GetCurrentValue());
+            }
+            else
+            {
+                Debug.LogWarning($"{component.name} has no CombinationCylinder attached!");
+                currentValues.Add(-1); // placeholder if missing
+            }
+        }
+
+        return currentValues;
+    }
+
+    private void CheckCombination()
+    {
+        List<int> playerInput = GetCurrentCombination();
+
+
+        int combinedPlayerInput = int.Parse(string.Join("", playerInput));
+        int combinedCorrectCombo = int.Parse(string.Join("", correctCombination));
+        if (combinedPlayerInput == combinedCorrectCombo)
+        {
+            SolvePuzzle();
+            this.enabled = false;
+        }
+    }
+}
+
---
+++ Assets/Puzzle/PuzzleSO/MultiInputCombinationPuzzle.cs
@@ -0,0 +1,51 @@
+using NUnit.Framework;
+using System;
+using UnityEngine;
+
+public class MultiInputCombinationPuzzle : Puzzle
+{
+    [SerializeField] private InputCombination[] inputCombinationPuzzles;
+    [SerializeField] private GameObject[] flowers;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public override void StartPuzzle()
+    {
+        base.StartPuzzle();
+
+    }
+    public override void SolvePuzzle()
+    {
+        base.SolvePuzzle();
+        puzzleData.isSolved = true;
+    }
+    void Start()
+    {
+        foreach (InputCombination puzzle in inputCombinationPuzzles)
+        {
+
+            if (puzzle.instanceID == InputCombination.PuzzleID.West) {
+                puzzle.OnComponentRotated += ChangeEnvironment;
+            }
+        }
+        //puzzle.OnComponentRotated += CheckSolutionProximity;
+   
+    }
+
+    private void ChangeEnvironment(InputCombination inputCombination)
+    {
+        int currentIndex = System.Array.IndexOf(inputCombination.combinationComponents, inputCombination.currentComponent);
+ 
+        if (inputCombination.currentComponent.GetComponent<CombinationCylinder>().GetCurrentValue() == inputCombination.correctCombination[currentIndex])
+        {
+            flowers[currentIndex].SetActive(true);
+        } else
+        {
+            flowers[currentIndex].SetActive(false);
+        }
+    }
+
+    private void CheckSolutionProximity()
+    {
+        Debug.Log("Checking how close the combination is to the solution...");
+    }
+
+}
