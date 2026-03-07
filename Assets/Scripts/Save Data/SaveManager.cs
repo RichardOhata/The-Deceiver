4b3075efb56323d640f29d5123b29ef341d5032d
RichardOhata <richardohata@gmail.com>
2026-03-06 20:54:58 -0800

    implemented custom save loading icon

--- Assets/Scripts/Save Data/SaveManager.cs
+++ Assets/Scripts/Save Data/SaveManager.cs
@@ -1,14 +1,19 @@
 using UnityEngine;
 using System.IO;
+using System.Collections;
+using System.Diagnostics;
 
 [DefaultExecutionOrder(-100)]
 public class SaveManager : MonoBehaviour
 {
     private string saveFilePath;
-
     public static SaveManager Instance;
-
     public SaveData currentData = new SaveData();
+
+
+    [Header("UI Reference")]
+    public GameObject saveIcon;
+    public float extraPaddingTime = 0.5f;
 
     private void Awake()
     {
@@ -16,16 +21,39 @@ public class SaveManager : MonoBehaviour
         else Destroy(gameObject);
 
         saveFilePath = Application.persistentDataPath + "/SaveData.json";
-
+        if (saveIcon != null) saveIcon.SetActive(false);
         LoadGame();
     }
 
 
     public void SaveGame()
     {
+        StartCoroutine(DynamicSaveSequence());
+    }
+
+    private IEnumerator DynamicSaveSequence()
+    {
+     
+        Stopwatch timer = new Stopwatch();
+        timer.Start();
+
+        if (saveIcon != null) saveIcon.SetActive(true);
+
+   
         string json = JsonUtility.ToJson(currentData, true);
         File.WriteAllText(saveFilePath, json);
-        Debug.Log("Game successfully saved to: " + saveFilePath);
+
+       
+        timer.Stop();
+        float saveDurationSeconds = (float)timer.Elapsed.TotalSeconds;
+        float totalDisplayTime = saveDurationSeconds + extraPaddingTime;
+
+        UnityEngine.Debug.Log($"Save took {saveDurationSeconds:F4}s. Showing icon for {totalDisplayTime:F4}s.");
+
+   
+        yield return new WaitForSeconds(totalDisplayTime);
+
+        if (saveIcon != null) saveIcon.SetActive(false);
     }
 
     public void LoadGame()
@@ -34,12 +62,20 @@ public class SaveManager : MonoBehaviour
         {
             string json = File.ReadAllText(saveFilePath);
             currentData = JsonUtility.FromJson<SaveData>(json);
-            Debug.Log("Game successfully loaded from: " + saveFilePath);
+            UnityEngine.Debug.Log("Game successfully loaded from: " + saveFilePath);
         }
         else
         {
-            Debug.LogWarning("No save file found! Creating new save data.");
+            UnityEngine.Debug.LogWarning("No save file found! Creating new save data.");
             currentData = new SaveData();
         }
+    }
+
+    [ContextMenu("Reset Save Data to Default")]
+    public void ResetToDefault()
+    {
+        currentData = new SaveData();
+        SaveGame();
+        UnityEngine.Debug.Log("All save data has been reset to defaults via Inspector.");
     }
 }
